97bedc4209e84555a6c47307d51fcb22086be55c
Protoway <dvnsrz12@gmail.com>
2020-04-15 19:48:15 -0400

    Buttons

--- pages/g16/g16.ts
+++ pages/g16/g16.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Chart } from 'chart.js'
 import { G16cPage } from '../g16c/g16c'
+import { G16ncPage } from '../g16nc/g16nc'
+import { G16sPage } from '../g16s/g16s'
 
 @Component({
   selector: 'page-g16',
@@ -15,5 +17,11 @@ export class G16Page {
 
   g16c(){
   this.navCtrl.push(G16cPage);
+  }
+  g16nc(){
+  this.navCtrl.push(G16ncPage);
+  }
+  g16s(){
+  this.navCtrl.push(G16sPage);
   }
 }
