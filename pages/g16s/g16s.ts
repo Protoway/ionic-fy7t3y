110cb5106d75a14a42f40794e2a1dde3347d97a3
Protoway <dvnsrz12@gmail.com>
2020-04-18 18:14:02 -0400

    Full switch to function in all pages

--- pages/g16s/g16s.ts
+++ pages/g16s/g16s.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Chart } from 'chart.js'
+import {GraphBuilders} from '../../app/graph-builders';
 
 @Component({
   selector: 'page-g16s',
@@ -8,32 +9,17 @@ import { Chart } from 'chart.js'
 })
 export class G16sPage {
   @ViewChild('fillChart') fillChart;
-  pie: any;
+  private builder:GraphBuilders = new GraphBuilders();
   colorArray: any;
+  inputdata: number[] = [25, 49, 26];
   constructor(public navCtrl: NavController) {
 
   }
   ionViewDidEnter() {
-    this.createBarChart();
-  }
-
-  createBarChart() {
     this.colorArray = [];
     for (let i = 0; i < 3; i++) {
     this.colorArray.push('#' + Math.floor(Math.random() * 16777215).toString(16));
-  }
-    this.pie = new Chart(this.fillChart.nativeElement, {
-      type: 'pie',
-      data: {
-        labels: ['Empty', 'Taken', 'Closed'],
-        datasets: [{
-          label: 'Viewers in millions',
-          data: [33, 33, 34],
-          backgroundColor: this.colorArray, // array should have same number of elements as number of dataset
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      }
-    });
+    }
+    this.builder.createPieChart(this.colorArray, this.inputdata, this.fillChart);
   }
 }
