9b21d3628cad3412092b09e904ab78349d7faf76
Protoway <dvnsrz12@gmail.com>
2020-04-18 16:25:18 -0400

    Successfully using class instead of repetition

--- app/graph-builders.ts
+++ app/graph-builders.ts
@@ -1,14 +1,14 @@
 import { Chart } from 'chart.js'
 
 export class GraphBuilders {
-  public createPieChart(carr, inputdata, ctx){
+  public createPieChart(carr:number[], inputdata:number[], ctx){
     var pie = new Chart(ctx.nativeElement, {
       type: 'pie',
       data: {
         labels: ['Empty', 'Taken', 'Closed'],
         datasets: [{
           label: 'Viewers in millions',
-          data: [33, 33, 34],
+          data: inputdata,
           backgroundColor: carr, // array should have same number of elements as number of dataset
           borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
           borderWidth: 1
--- pages/g16nc/g16nc.ts
+++ pages/g16nc/g16nc.ts
@@ -10,12 +10,16 @@ import {GraphBuilders} from '../../app/graph-builders';
 export class G16ncPage {
   @ViewChild('fillChart') fillChart;
   private builder:GraphBuilders = new GraphBuilders();
-  colorArray: [5, 46, 23];
-  inputdata: [3, 5, 5]
+  colorArray: any;
+  inputdata: number[] = [25, 50, 25];
   constructor(public navCtrl: NavController) {
 
   }
   ionViewDidEnter() {
+    this.colorArray = [];
+    for (let i = 0; i < 3; i++) {
+    this.colorArray.push('#' + Math.floor(Math.random() * 16777215).toString(16));
+    }
     this.builder.createPieChart(this.colorArray, this.inputdata, this.fillChart);
   }
   /*
